951ae54e9a24237fd78c62088e160725c4ed9113
Harshit Gupta <harshitgupta0910@gmail.com>
2025-08-03 22:10:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
 import './App.css';
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
 import Home from './components/Home';
 import EditorPage from './components/EditorPage';
 import { Toaster } from 'react-hot-toast';
 
+// ProtectedRoute component
+const ProtectedRoute = ({ children }) => {
+  const isAuthenticated = !!localStorage.getItem("token"); // or whatever you use
+  return isAuthenticated ? children : <Navigate to="/" replace />;
+};
+
 function App() {
   return (
-    <>
-    <div>
-      <Toaster  position='top-center'></Toaster>
-    </div>
-    <Routes>
-     <Route path='/' element={ <Home /> } />
-     <Route path='/editor/:roomId' element={ <EditorPage /> } />
-    </Routes>
-    </>
+    <BrowserRouter>
+      <div>
+        <Toaster position='top-center' />
+      </div>
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route
+          path='/editor/:roomId'
+          element={
+            <ProtectedRoute>
+              <EditorPage />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
