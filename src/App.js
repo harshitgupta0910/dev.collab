940bce73fb4c5923c2a4de6ad865c8ba03609bc1
Harshit Gupta <harshitgupta0910@gmail.com>
2025-08-03 22:13:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Routes, Route } from "react-router-dom";
 import Home from './components/Home';
 import EditorPage from './components/EditorPage';
 import { Toaster } from 'react-hot-toast';
-
 function App() {
   return (
     <>
